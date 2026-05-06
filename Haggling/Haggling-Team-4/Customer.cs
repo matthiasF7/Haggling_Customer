dbf3adeeeb57fbcc8dc0a1a2ab0b3da9cf149e18
matthiasF7 <m.frei@students.htl-leonding.ac.at>
2026-05-06 13:24:29 +0200

    start implementing

---
+++ Haggling/Haggling-Team-4/Customer.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlTypes;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Haggling_Team_4
+{
+    internal class Customer : ICustomer
+    {
+        public Customer(decimal startMoney, List<Product.ProductTypeEnum> Likes, List<Product.ProductTypeEnum> Dislikes) {
+            
+        }
+
+
+        protected abstract bool DecideToBuy(Product product, decimal prize)
+        {
+
+        }
+
+        public abstract decimal negotiatePrize(Product product, Vendor vendor)
+        {
+
+        }
+
+
+
+    }
+
+
+}
---
+++ Haggling/Haggling-Team-4/ICustomer.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.SymbolStore;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace Haggling_Team_4
+{
+    internal interface ICustomer
+    {
+        public decimal Money {  get; protected set; }
+        public List<Product.ProductTypeEnum> Likes { get; }
+        public List<Product.ProductTypeEnum> Dislikes { get; }
+        public Dictionary<Vendor, Product> Bought { get; protected set; }
+
+        protected abstract bool DecideToBuy(Product product, decimal prize);
+
+        public abstract decimal negotiatePrize(Product product, Vendor vendor);
+
+        private int likesVendor(Vendor vendor)
+        {
+            int count = 0;
+            foreach(Vendor v in Bought.Keys)
+            {
+                if(v == vendor)
+                {
+                    count++;
+                }
+            }
+            return count;
+        }
+
+    }
+}
