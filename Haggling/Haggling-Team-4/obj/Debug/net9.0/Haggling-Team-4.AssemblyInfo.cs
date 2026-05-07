08fad6710240d2aeaf43ebf2eb7cadd9e0c3fa29
spieli11111011010 <tobi.spiegelei@gmail.com>
2026-05-07 14:09:28 +0200

    change visibility of properties

--- Haggling/Haggling-Team-4/obj/Debug/net9.0/Haggling-Team-4.AssemblyInfo.cs
+++ Haggling/Haggling-Team-4/obj/Debug/net9.0/Haggling-Team-4.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Haggling-Team-4")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6839bb209ae726bc74d8ea5efca93cbbd34740ed")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3c80b9aa8abe3aa5d9b31788da8860ac5a9f07e3")]
 [assembly: System.Reflection.AssemblyProductAttribute("Haggling-Team-4")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Haggling-Team-4")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
